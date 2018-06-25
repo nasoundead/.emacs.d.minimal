9784dba338ddab138d4d1499710e57fba6dc1e69
wanghaibo <nasoundead@163.com>
2018-06-25 22:41:03 +0800

    a

--- init.el
+++ init.el
@@ -38,6 +38,30 @@ re-downloaded in order to locate PACKAGE."
         (package-refresh-contents)
         (require-package package min-version t)))))
 
+(setq make-backup-files nil)
+;; 设定不产生备份文件
+(setq auto-save-mode nil)
+;;自动保存模式
+(setq-default make-backup-files nil)
+;; 不生成临时文件
+;; 隐藏滚动栏和菜单，最好用下面三条
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(setq visible-bell t)
+;;关闭烦人的出错时的提示声
+(setq inhibit-startup-message t)
+;;关闭emacs启动时的画面
+(setq gnus-inhibit-startup-message t)
+;;关闭gnus启动时的画面
+(fset 'yes-or-no-p 'y-or-n-p)
+(show-paren-mode t)
+(setq frame-title-format "emacs@%b")
+(setq kill-ring-max 200)
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 8)
+(setq tab-stop-list ())
+
+
 (require-package 'use-package)
 (require-package 'wgrep)
 (require-package 'diminish)
