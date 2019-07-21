77c4d4f57607af2e504a8fd770ea35dc1ee5ca9c
nasoundead <nasoundead@163.com>
2019-07-21 21:25:40 +0800

    lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -78,7 +78,16 @@
     (setq mode-line-format nil))
   )
 
-(use-package dap-mode)
+(use-package lsp-java :ensure t :after lsp
+  :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+  :ensure t :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+;; (use-package dap-java :after (lsp-java))
 
 (provide 'init-lsp)
 
